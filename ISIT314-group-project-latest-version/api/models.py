f122c9c66a49747bb33a36adae5644ba9b09156e
shadow29398 <shadow29398@gmail.com>
2026-05-24 21:12:09 +1000

    Refactor User class to Candidate and add user_type

--- ISIT314-group-project-latest-version/api/models.py
+++ ISIT314-group-project-latest-version/api/models.py
@@ -1,33 +1,49 @@
 # ================= models.py =================
 from django.db import models
 
-class User(models.Model):
+
+class Candidate(models.Model):
+
+# choice of being a candidate or a company, default is candidate
+    USER_TYPE_CHOICES = [
+        ('Candidate', 'Candidate'),
+        ('Company', 'Company'),
+    ]
+
     name = models.CharField(max_length=100)
     contact = models.CharField(max_length=100)
+
+    user_type = models.CharField(
+        max_length=20,
+        choices=USER_TYPE_CHOICES,
+        default='Candidate'
+    )
+
     education = models.CharField(max_length=100)
     major = models.CharField(max_length=100)
     experience = models.IntegerField()
     skills = models.JSONField()
-    #Coice of being a candidate or a company
-    Candidate_or_Company = [
-        ('Candidate', 'Candidate'),
-        ('Company', 'Company'),
-    ]
-    # Allowed work modes for matching jobs
+
+    # allowed workk mode for matching jobs
     WORK_MODE_CHOICES = [
         ('Remote', 'Remote'),
-        ('On-site', 'On-site'), 
+        ('On-site', 'On-site'),
         ('Hybrid', 'Hybrid'),
     ]
+
     preferred_working_mode = models.CharField(
         max_length=10,
         choices=WORK_MODE_CHOICES
     )
+
     preferred_location = models.CharField(max_length=100)
-    #Membership?
+
+    # membership status 
     membership = models.BooleanField(default=False)
 
-#NEEDS TO BE OBJECT ORIENTED
+    def __str__(self):
+        return self.name
+
 
 class Job(models.Model):
     title = models.CharField(max_length=100)
@@ -39,4 +55,7 @@ class Job(models.Model):
     mode = models.CharField(max_length=50)
     location = models.CharField(max_length=100)
 
-#NEEDS TO BE OBJECT ORIENTED
+    membership = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
